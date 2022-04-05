15f6b355bd14313eddca362b60b7e04e6d07e3c3
Michael Irimus <michaelirimus@gmail.com>
2022-04-05 19:58:09 +0200

    initial commit

---
+++ src/main/java/leachimsumiri/anyline/VideoFrameExtractionService/VideoFrameExtractionServiceApplication.java
@@ -0,0 +1,13 @@
+package leachimsumiri.anyline.VideoFrameExtractionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoFrameExtractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoFrameExtractionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leachimsumiri/anyline/VideoFrameExtractionService/VideoFrameExtractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package leachimsumiri.anyline.VideoFrameExtractionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoFrameExtractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
